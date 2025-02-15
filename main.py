acc56e5ba9b5c16fa9b406bce7eb1b03c628cb71
Đào Đức Lam <daoduclam0620@gmail.com>
2025-02-16 11:43:13 +1400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import mnist_loader
+import network
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net_exercises = network.Network([784, 10])
+net_exercises.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)

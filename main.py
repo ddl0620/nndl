1e2d5902d412242df71141f1b8c67e629685788c
Đào Đức Lam <s4019052@rmit.edu.vn>
2025-02-18 15:14:45 +0700

    18225

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import mnist_loader
 import network
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net_exercises = network.Network([784, 10])
-net_exercises.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+#net_exercises = network.Network([784, 10])
+#net_exercises.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
 net = network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
